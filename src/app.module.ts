9b4409697dfc3530b7abd1df3306bda0799d2a70
Matheus Fernandes Pinheiro <mpinheiro@iblueconsulting.com.br>
2022-04-06 23:51:36 -0300

    feat app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+import * as SqliteConfig from '../database/config/sqlite';
+import { HttpExceptionFilter } from './exception-filters/http-exception-filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(SqliteConfig),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
