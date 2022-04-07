e17c8d7cc7726748538b10e055cadc58bb23406e
Matheus Fernandes Pinheiro <mpinheiro@iblueconsulting.com.br>
2022-04-07 09:22:53 -0300

    feat exception filters

---
+++ src/exception-filters/http-exception-filter.ts
@@ -0,0 +1,23 @@
+import {
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpException,
+  Catch,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      error: exception.getResponse(),
+    });
+  }
+}
