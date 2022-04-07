cc34a490336af97a29d5db8198eb7ed8a3762836
Matheus Fernandes Pinheiro <mpinheiro@iblueconsulting.com.br>
2022-04-07 19:40:44 -0300

    fixing add guard to session controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './shared/local-auth.guard';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('sessions')
   async login(@Request() req: any): Promise<{
     access_token: string;
